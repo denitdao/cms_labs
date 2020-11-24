8d0325b945c93eaec35b8f59a52f6d8bc29debb4
Denys <dodenisa@gmail.com>
2020-11-24 23:36:46 +0200

    Made macros recognition for tiles insertion

--- lab_N/app/Http/Traits/MacroEncoderTrait.php
+++ lab_N/app/Http/Traits/MacroEncoderTrait.php
@@ -17,7 +17,7 @@ trait MacroEncoderTrait
                     break;
                 }
                 case 'tiles': {
-
+                    self::tilesReplaceUa($content, $macro, @$commands[1], @$commands[2]);
                     break;
                 }
                 default:
@@ -38,7 +38,7 @@ trait MacroEncoderTrait
                     break;
                 }
                 case 'tiles': {
-
+                    self::tilesReplaceEn($content, $macro, @$commands[1], @$commands[2]);
                     break;
                 }
                 default:
@@ -64,5 +64,53 @@ trait MacroEncoderTrait
         $page = Page::firstWhere('code', $code);
         if(!is_null($page))
             $content = preg_replace('/~{\s*'.$macro.'\s*}~/','<a href="/'.$page->code.'/en" style="background-color: transparent; color: rgb(31, 31, 38);">'.($text ?? $page->caption_en).'</a>', $content);
+    }
+
+    private static function tilesReplaceUa(&$content, $macro, $code, $amount){
+        $page = Page::getPage($code);
+        if(!isset($page->items)) return;
+        $tiles ='<div class="mdl-cell mdl-cell--12-col mdl-color-text--grey-800 mdl-grid">';
+        foreach($page->items->slice(0, $amount) as $item) {
+            $tiles .=
+            '<div class="mdl-cell mdl-cell--4-col-tablet mdl-cell--4-col-desktop mdl-card mdl-shadow--3dp">
+                <div class="mdl-card__title mdl-card--expand my-card-background" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0,0,0,0.49) 100%), url('.asset('storage/images/'.($item->page_photo_path ?? 'default.png')).');">
+                    <h5 class="mdl-card__title-text">'.($item->caption_ua ?? "").'</h5>
+                </div>
+                <div class="mdl-card__supporting-text">'.($item->intro_ua ?? "").'</div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/'.($item->alias_of_page()->first()->code ?? $item->code).'">
+                        Дізнатися більше
+                    </a>
+                </div>
+            </div>';
+        }
+        $tiles .= '</div>';
+        if(!is_null($tiles)) {
+            $content = preg_replace('/~{\s*'.$macro.'\s*}~/',$tiles, $content);
+        }
+    }
+
+    private static function tilesReplaceEn(&$content, $macro, $code, $amount){
+        $page = Page::getPage($code);
+        if(!isset($page->items)) return;
+        $tiles ='<div class="mdl-cell mdl-cell--12-col mdl-color-text--grey-800 mdl-grid">';
+        foreach($page->items->slice(0, $amount) as $item) {
+            $tiles .=
+                '<div class="mdl-cell mdl-cell--4-col-tablet mdl-cell--4-col-desktop mdl-card mdl-shadow--3dp">
+                <div class="mdl-card__title mdl-card--expand my-card-background" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0,0,0,0.49) 100%), url('.asset('storage/images/'.($item->page_photo_path ?? 'default.png')).');">
+                    <h5 class="mdl-card__title-text">'.($item->caption_en ?? "").'</h5>
+                </div>
+                <div class="mdl-card__supporting-text">'.($item->intro_en ?? "").'</div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/'.($item->alias_of_page()->first()->code ?? $item->code).'/en">
+                        Read more
+                    </a>
+                </div>
+            </div>';
+        }
+        $tiles .= '</div>';
+        if(!is_null($tiles)) {
+            $content = preg_replace('/~{\s*'.$macro.'\s*}~/',$tiles, $content);
+        }
     }
 }
